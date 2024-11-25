94de000ce61486ee8e4918e79d08f24d7f517df7
Alex <sergeyev.alex@gmail.com>
2024-11-25 17:56:51 -0500

    add new logo component

---
+++ resources/views/components/application-logo-horizontal.blade.php
@@ -0,0 +1,8 @@
+<div class="inline-flex items-center space-x-2">
+    <x-application-logo-svg></x-application-logo-svg>
+    <span
+        style="font-family: 'Asap Condensed', sans-serif; color: #00786c; font-weight: bold; font-size: 26px; padding-top: 2px;"
+    >{{ getenv('app_logo_text') }}</span>
+{{--    Customize Toolbar…--}}
+</div>
+
